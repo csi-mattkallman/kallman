b495d664a98279c65e44900d91a4f06945fb84d9
csi-mattkallman <mkallman@compliancesystems.com>
2021-02-10 13:29:32 -0500

    📦 NEW: Added colors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
+const chalk = require('chalk');
+ 
+console.log(chalk.blue('Hello world!'));
 
 welcome({
-    title: pkgJSON.name, 
+    title: `Matt Kallman`, 
     tagLine: pkgJSON.description, 
     version: pkgJSON.version,
     bgColor: `#FADC00`,
@@ -12,9 +15,7 @@ welcome({
     clear: true
 });
 
-console.log(`Matt Kallman
-
-Matt is a Software Business Professional based in Grand Rapids, Michigan, US.  
-He is also a member of the local County elected board.  See accesskent.com.
+console.log(`${chalk.italic(`Matt is a Software Business Professional based in Grand Rapids, Michigan, US.  
+He is also a member of the local County elected board.  See accesskent.com.`)}
 
 `);
