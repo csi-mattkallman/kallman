2e4e6ca9dc035b873faa93c8e1e5d5c4e37b614b
csi-mattkallman <mkallman@compliancesystems.com>
2021-02-10 11:49:15 -0500

    First version

---
+++ index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+console.log(`Matt Kallman
+
+Matt is a Software Business Professional based in Grand Rapids, Michigan, US.  
+He is also a member of the local County elected board.  See accesskent.com.
+
+`);
